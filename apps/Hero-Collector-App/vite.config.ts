44344090ea3ebbf6a51b78065dcc9ffbb1dc1c38
Remedeae <sanna.m.blomdahl@gmail.com>
2026-01-29 02:22:01 +0100

    root directory for frontend html

--- apps/Hero-Collector-App/vite.config.ts
+++ apps/Hero-Collector-App/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: resolve(__dirname),
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 5173,
