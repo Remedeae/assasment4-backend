eb6d1b9dab36b10f20199a03383f396d257419f4
Remedeae <sanna.m.blomdahl@gmail.com>
2026-02-05 04:59:51 +0100

    frontend test updates forgotten to save

--- apps/Hero-Collector-App/src/components/globals/Header.tsx
+++ apps/Hero-Collector-App/src/components/globals/Header.tsx
@@ -2,22 +2,33 @@ import ActiveUser from "./header/ActiveUser";
 import Nav from "./header/nav";
 import { useAdminToggle } from "../../storage/adminToggleStore";
 import { useAuthStore } from "../../storage/authStore";
+import { api } from "../../../api/axios";
+import type { PlayerOutput } from "@heroapp/shared";
+import { useState } from "react";
 export default function Header() {
   const isTrueAdmin = useAuthStore((s) => s.isTrueAdmin);
   const isAdmin = useAdminToggle((s) => s.isAdmin);
   const setIsAdmin = useAdminToggle((s) => s.setIsAdmin);
+
+  const [test, setTest] = useState<PlayerOutput | null>(null);
+
+  const handleTest = async () => {
+    const res = await api<PlayerOutput>("get", "/allUsers");
+    setTest(res);
+  };
 
   return (
     <header>
       <Nav />
       <ActiveUser />
       {isTrueAdmin ? (
-        <button className="admintoggle"
-         onClick={() => setIsAdmin(!isAdmin)}>
+        <button className="admintoggle" onClick={() => setIsAdmin(!isAdmin)}>
           Swap to {!isAdmin && "Admin"}
           {isAdmin && "User"} view
         </button>
       ) : null}
+      <button onClick={handleTest}>test refersh</button>
+      <h1>{test?.userName}</h1>
     </header>
   );
 }
