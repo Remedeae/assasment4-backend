80b0e1bc9b10b908ecb2c016a9c2e1c1a04c7e8a
Remedeae <sanna.m.blomdahl@gmail.com>
2026-02-05 04:32:49 +0100

    purging jsonwebtoken

--- apps/Backend/dist/middleware/auth/tokenCheck.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-export declare const requiresAuth: (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=tokenCheck.d.ts.map
--- apps/Backend/src/routes/auth/callback.ts
+++ apps/Backend/src/routes/auth/callback.ts
@@ -1,28 +1,9 @@
 import "dotenv/config";
 import { Router } from "express";
-import jwt from "jsonwebtoken";
 import { frontendURL } from "@heroapp/shared";
-import { error } from "node:console";
 
 const router = Router();
 
-router.get("", (req, res) => {
-  const user = req.oidc.user;
-
-  if (!user) {
-    return res.redirect(`${frontendURL}?error=login_failed`);
-  }
-  const token = jwt.sign(
-    {
-      auth0Id: user.sub,
-      userName: user.nickName,
-      email: user.email,
-      roles: user["https://remedeae-hero-collector/roles"] || [],
-    },
-    `${process.env.JWT_SECRET}`,
-    { expiresIn: 300 },
-  );
-  res.redirect(`${frontendURL}/home?token=${token}`);
-});
+router.get("", (req, res) => {});
 
 export default router;
