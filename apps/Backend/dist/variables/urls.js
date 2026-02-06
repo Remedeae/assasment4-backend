8398907e733397336285212fdaca37c2e6905d36
Remedeae <sanna.m.blomdahl@gmail.com>
2026-02-06 08:14:11 +0100

    fixed up some env variable inconsistencies

--- apps/Backend/dist/variables/urls.js
+++ apps/Backend/dist/variables/urls.js
@@ -1,11 +1,11 @@
-export const frontendURL = process.env.NODE_ENV === "dev"
+export const frontendURL = process.env.MODE === "development"
     ? String(process.env.FRONTEND_URL_DEV)
-    : process.env.NODE_ENV === "production"
+    : process.env.MODE === "production"
         ? String(process.env.FRONTEND_URL_PROD)
         : "http://localhost:5173";
-export const backendURL = process.env.NODE_ENV === "dev"
+export const backendURL = process.env.MODE === "development"
     ? String(process.env.BACKEND_URL_DEV)
-    : process.env.NODE_ENV === "production"
+    : process.env.MODE === "production"
         ? String(process.env.BACKEND_URL_PROD)
         : "http://localhost:3000";
 //# sourceMappingURL=urls.js.map
